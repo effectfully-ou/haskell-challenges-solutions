8ef637a405efb405f83cdf504f6d0620cf21d740
effectfully <effectfully@gmail.com>
2022-06-12 01:55:46 +0300

    Added a solution to 'h8-foldr-foldlprime'

--- h8-foldr-foldlprime/src/Lib.hs
+++ h8-foldr-foldlprime/src/Lib.hs
@@ -1,9 +1,27 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Lib
     ( foldr
     ) where
 
-import           Data.Foldable (foldl')
-import           Prelude       hiding (foldr)
+import           Control.Concurrent
+import           Data.Foldable      (foldl')
+import           Data.Function
+import           Data.Functor
+import           Prelude            hiding (foldr)
+import           System.IO.Unsafe
 
 foldr :: Foldable t => (a -> b -> b) -> b -> t a -> b
-foldr = undefined
+foldr f z xs = unsafePerformIO $ do
+    request  <- newEmptyMVar
+    response <- newEmptyMVar
+    _ <- forkIO $ do
+        let f' r x = unsafePerformIO $ do
+                putMVar response $ Just x
+                r <$ takeMVar request
+            z' = putMVar response Nothing
+        foldl' f' z' xs
+    fix $ \r ->
+        takeMVar response >>= \case
+            Nothing -> pure z
+            Just x  -> f x <$> unsafeInterleaveIO (putMVar request () *> r)
